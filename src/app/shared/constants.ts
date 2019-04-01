a3131a70505b36a323233a0fc0fe3d3a9cf475d1
Arjen Brandenburgh <arjen.brandenburgh@gmail.com>
2019-04-01 12:06:54 +0200

    Social icons change and added Medium

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,10 +1,31 @@
 export class Constants {
   public static get SOCIAL_LINKS(): any {
-    return {
-      'facebook' : 'https://www.facebook.com/ArjenBrandenburgh',
-      'twitter' : 'https://twitter.com/ArjenBrand',
-      'github' : 'https://github.com/arjenbrandenburgh/',
-      'linkedin' : 'https://www.linkedin.com/in/arjenbrandenburgh/'
-    };
+    return [
+      {
+        name: 'Github',
+        icon: 'fa-github',
+        url: 'https://www.linkedin.com/in/arjenbrandenburgh/'
+      },
+      {
+        name: 'Medium',
+        icon: 'fa-medium',
+        url: 'https://medium.com/@arjenbrandenburgh/'
+      },
+      {
+        name: 'Facebook',
+        icon: 'fa-facebook',
+        url: 'https://www.facebook.com/ArjenBrandenburgh'
+      },
+      {
+        name: 'Twitter',
+        icon: 'fa-twitter',
+        url: 'https://twitter.com/ArjenBrand'
+      },
+      {
+        name: 'LinkedIn',
+        icon: 'fa-linkedin',
+        url: 'https://www.linkedin.com/in/arjenbrandenburgh/'
+      },
+    ];
   }
 }
